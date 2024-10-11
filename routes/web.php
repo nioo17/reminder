a89ebec4ffebddd413f6581b5e1be421ff954349
poccalopa <putriayulestari621@gmail.com>
2024-10-11 16:48:07 +0800

    tampilan login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('user.login');
+});
+
+Route::get('/dashboard', function () {
     return view('layouts.main');
 });
 
