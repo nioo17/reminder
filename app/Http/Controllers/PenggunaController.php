354b0d621c7950051b73be1ff66ade10c167df12
poccalopa <putriayulestari621@gmail.com>
2024-10-15 16:02:31 +0800

    data pengguna

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -10,12 +10,9 @@ class PenggunaController extends Controller
     public function index(Request $request)
     {
         $title = 'Data Pengguna';
-        $q = $request->query('q');
-        $penggunas = Pengguna::where('judul_buku', 'like', '%' . $q . '%')
-            // ->leftjoin('tb_kategori', 'tb_kategori.id_kategori', 'tb_buku.id_kategori')
-            ->paginate(15)
-            ->withQueryString();
+        // $q = $request->query('q');
+        $penggunas = Pengguna::get();
         $no = $penggunas->firstItem();
-        return view('pengguna.index', compact('title', 'penggunas', 'q', 'no'));
+        return view('pengguna.datapengguna', compact('title', 'penggunas', 'no'));
     }
 }
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,7 +4,6 @@
       <img src="templates/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Reminder</span>
     </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
@@ -23,7 +22,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="true">
             <!-- Dashboard -->
             <li class="nav-item">
-                <a href="/" class="nav-link">
+                <a href="/dashboard" class="nav-link">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>
                         Dashboard
