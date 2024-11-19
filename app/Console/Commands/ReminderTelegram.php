d3f1dffc996c1f77c30a4beb18ce60d1ffbb5cc5
TresnaAgustina <tresnaa001@gmail.com>
2024-11-19 14:47:59 +0800

    update environment variables and enhance Telegram reminder command

--- app/Console/Commands/ReminderTelegram.php
+++ app/Console/Commands/ReminderTelegram.php
@@ -8,6 +8,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class ReminderTelegram extends Command
 {
@@ -15,27 +16,35 @@ class ReminderTelegram extends Command
 
     public function handle()
     {
-        $today = Carbon::today();
-        $token = env('TELEGRAM_BOT_TOKEN');
-        $chatId = env('TELEGRAM_CHAT_ID');
-        $events = DB::table('events')
-            ->whereDate('tanggal', '=', $today)
-            ->where('is_sent', false)
-            ->get();
+        try {
+            // ambil tanggal besok
+            $tomorrow = Carbon::tomorrow();
+            $token = env('TELEGRAM_BOT_TOKEN');
+            $chatId = env('TELEGRAM_CHAT_ID');
+            $events = DB::table('events')
+                ->whereDate('tanggal', '=', $tomorrow)
+                ->where('is_sent', false)
+                ->get();
 
-        foreach ($events as $event) {
-            // $message = "📢 Reminder: {$event->judul}\n\n"
-            //          . "{$event->pesan}\n"
-            //          . "Tanggal: {$event->tanggal}"; 
+                Log::info($events);
 
-            $message = "Reminder: {$event->judul}";
+            foreach ($events as $event) {
+                // $message = "📢 Reminder: {$event->judul}\n\n"
+                //          . "{$event->pesan}\n"
+                //          . "Tanggal: {$event->tanggal}"; 
 
-            Http::post("https://api.telegram.org/bot{$token}/sendMessage", 
-            [
-                'chat_id' => $chatId,
-                'text' => $message,
-                'parse_mode' => 'Markdown',
-            ]);
+                $message = "Reminder: {$event->judul}";
+
+                Http::post("https://api.telegram.org/bot{$token}/sendMessage", 
+                [
+                    'chat_id' => $chatId,
+                    'text' => $message,
+                    'parse_mode' => 'Markdown',
+                ]);
+            }
+            Log::info('Reminder sent successfully');
+        } catch (\Throwable $th) {
+            Log::info($th->getMessage());
         }
     }
 }
